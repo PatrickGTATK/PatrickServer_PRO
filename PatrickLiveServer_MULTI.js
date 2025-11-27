894f5965d255c7b8f16d40bdc36f9986c4069ca4
PatrickGTATK <telateen1@gmail.com>
2025-11-27 10:38:24 -0300

    Update PatrickLiveServer_MULTI.js

--- PatrickLiveServer_MULTI.js
+++ PatrickLiveServer_MULTI.js
@@ -2,6 +2,6 @@ import "dotenv/config";
 import http from "http";
 import express from "express";
 import WebSocket, { WebSocketServer } from "ws";
-import WebcastPushConnection, { SignConfig } from "./tiktok-live-connector/index.js";
+import { WebcastPushConnection, SignConfig } from "tiktok-live-connector";
 const PORT = process.env.PORT || 8080;
 /* truncated for brevity */
